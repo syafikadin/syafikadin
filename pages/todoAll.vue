98faca9a9682034c39690c23ae588a13a909ec8f
syafikadin <syafik.adin222@gmail.com>
2022-04-21 05:13:55 +0700

    tes

---
+++ pages/todoAll.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-simple-table dark>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            ID
+          </th>
+          <th class="text-left">
+            Title
+          </th>
+          <th class="text-left">
+            isDone?
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in todoList"
+          :key="item.id"
+        >
+          <td>{{ item.id }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.isDone }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+    apollo: {
+        todoList: gql`
+            query allTodoQuery {
+                todoList {
+                    id
+                    isDone
+                    title
+                    userId
+                }
+            }
+        `
+    }
+}
+</script>
+
+<style>
+
+</style>
